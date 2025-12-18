7c1dc2c16a041f1833a026ae982c8496b4d8b8a0
alydinus <alykulovalydin@gmail.com>
2025-12-18 21:42:26 +0600

    Add RoleRepository interface for role management

---
+++ src/main/java/kg/spring/ort/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package kg.spring.ort.repository;
+
+import kg.spring.ort.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
+}
